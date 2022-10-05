3926966155effb91b383ddca18f0f4ebbc8ba933
Andrew Hexen <SyberHexen@gmail.com>
2022-10-05 04:54:07 -0700

    kiev: remove libdisplayconfig / vendor variant
    * included in display hal:
    qcom-caf/display/composer/Android.mk
    qcom-caf/display/config/display-product.mk
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Ie84e54dbdb0f8d2696e57af1e110297599b0edbf

--- device.mk
+++ device.mk
@@ -250,9 +250,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.lito \
-    libdisplayconfig \
     libdisplayconfig.qti \
-    libdisplayconfig.vendor \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
