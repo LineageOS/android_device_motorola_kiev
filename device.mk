6942c542ced36ef62c1fe7dff21fdbaf48c6e330
Wiktor Rudzki <zlewchan@outlook.com>
2025-03-05 10:12:45 +0530

    kiev: Commonize C2 blobs and configs
    These should be in sm7250-common where the actual service lives.
    
    Change-Id: I6d5e698c969ed015569611129be709f505e1095c

--- device.mk
+++ device.mk
@@ -88,8 +88,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_v3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
