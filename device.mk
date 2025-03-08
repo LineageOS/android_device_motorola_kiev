eed8f13437146b934e0d2ead38ce8ee50a72dca4
Giovanni Ricca <giovanniricca@proton.me>
2025-03-08 22:22:35 +0100

    kiev: configs: Move media folder out of configs
    Change-Id: Ie4c18335cfd1278f3196e3f8bceee00c009648ff

--- device.mk
+++ device.mk
@@ -78,7 +78,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # NFC
 PRODUCT_PACKAGES += \
