564ad2961c401ac621b78739728a607acb512c40
Giovanni Ricca <giovanniricca@proton.me>
2025-03-08 22:12:37 +0100

    kiev: Don't explicity build android.hardware.power-V1-ndk_platform
    Change-Id: I1aa6b3f10f683140b5c1cfd79cc44cdc002ee2bb

--- device.mk
+++ device.mk
@@ -31,10 +31,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl \
     firmware_aw_cali.bin_symlink
 
-# audio.primary.lito shim
-PRODUCT_PACKAGES += \
-    android.hardware.power-V1-ndk_platform.vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
